01eeb1aedb1d6a92eeacedb2cb509bf583cbd900
GsCabral <cabralgonzalo10@gmail.com>
2025-08-25 10:58:34 -0300

    distcreado

--- front/vite.config.js
+++ front/vite.config.js
@@ -1,9 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3006,
+    port: process.env.PORT ? parseInt(process.env.PORT) : 3006, // usa el puerto de Render o 3006 local
+    host: '0.0.0.0', // importante para Render
     open: true,
-  }})
+  }
+})
