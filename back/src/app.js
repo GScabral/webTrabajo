3494afc00e970e7be5bf978934c2c0fded5409dc
GsCabral <cabralgonzalo10@gmail.com>
2025-08-25 11:48:18 -0300

    cambios back

--- back/src/app.js
+++ back/src/app.js
@@ -13,21 +13,27 @@ server.name = "BACKEND";
 server.use(express.urlencoded({ extended: true, limit: '50mb' }));
 server.use(express.json({ limit: '50mb' }));
 server.use(cookieParser());
+const allowedOrigins = [
+    "http://localhost:3006",              // 👈 para desarrollo local
+    "https://webtrabajo-1.onrender.com"   // 👈 dominio de tu frontend en Render
+];
+
 server.use(cors({
-    origin: "http://localhost:3006",
+    origin: allowedOrigins,
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
     allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept", "Authorization"]
 }));
+
 server.use('/', routes)
 
 
-server.use((err,req,res,next)=>{
-    const status=err.status || 500;
-    const message=err.message || 'Internal Server Error';
+server.use((err, req, res, next) => {
+    const status = err.status || 500;
+    const message = err.message || 'Internal Server Error';
     console.error(err);
     res.status(status).send(message)
 })
 
 
-module.exports=server;
+module.exports = server;
