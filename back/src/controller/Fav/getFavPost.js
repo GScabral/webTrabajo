b0d7559d2a35b09ecc8d7f0c0c0863cb6aa9fb66
GsCabral <cabralgonzalo10@gmail.com>
2025-10-21 15:25:09 -0300

    cambiosfav

--- back/src/controller/Fav/getFavPost.js
+++ back/src/controller/Fav/getFavPost.js
@@ -1,16 +1,22 @@
-const { Favorite, Post } = require("../../db")
+const { Favorite, Post } = require("../../db");
 
 const getPostFav = async (user_id) => {
     if (!user_id) throw new Error("user_id es obligatorio");
+
     const favs = await Favorite.findAll({
-        where: { user_id, target_type: "post" },
-        include: [{ model: Post, as: "post", required: false }],
-        order: [["created_at", "DESC"]],
+        where: {
+            user_id,
+            target_type: "post"
+        },
+        include: [{
+            model: Post,
+            as: "post",
+            required: false
+        }],
+        order: [["created_at", "DESC"]]
     });
+
     return favs;
 };
-
-
-
 
 module.exports = getPostFav;
