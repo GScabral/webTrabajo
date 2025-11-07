a3860d248c407f35d890b0b6a6b30e45df00011e
GsCabral <cabralgonzalo10@gmail.com>
2025-11-07 09:50:51 -0300

    allbadges

--- front/src/utils/badges/AllBadges.jsx
+++ front/src/utils/badges/AllBadges.jsx
@@ -1,29 +1,28 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getAllBadges } from "../../redux/action/badgeAction";
 
-
 const AllBadges = () => {
-
-    const dispatch = useDispatch;
-    const badges = useSelector((state) => state.badgeState.allBadges)
-
-
+    const dispatch = useDispatch();
+    const badges = useSelector((state) => state.badgeState.allBadges);
 
     useEffect(() => {
-        dispatch(getAllBadges)
-    }, [dispatch])
+        dispatch(getAllBadges());
+    }, [dispatch]);
 
-    console.log("esto llega B:", badges)
+    console.log("esto llega B:", badges);
 
     return (
-
         <div>
             <h1>BADGES</h1>
+            {/* Opcional: muestra los badges */}
+            {badges && badges.length > 0 ? (
+                badges.map((b) => <p key={b.id}>{b.name}</p>)
+            ) : (
+                <p>No hay badges</p>
+            )}
         </div>
-    )
-
-}
-
+    );
+};
 
 export default AllBadges;
